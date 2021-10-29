4a959c934efef6067e387956068480d4ad86b88b
Carlos Cifuentes <cabetocifuentes@gmail.com>
2021-10-29 15:52:04 -0400

    Initial commit, includes splash screen working on all devices, keyboard control using Resign first responder, and other minor elements on UI.

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var ShoppingListName: UITextField!
+    
+    @IBAction func onSaveButtonPressed(_ sender: UIButton) {
+        self.ShoppingListName.resignFirstResponder()
+    }
+    
+    //Built in method
+    //Found on thenewboston youtube channel to hide the keyboard: https://www.youtube.com/watch?v=HI4JV7ClXB0
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
